6121bc1538efaefa517e6b3e9404f87b063a1dcb
Fabian Soto <fasop4@gmail.com>
2021-10-28 19:32:29 -0400

    implementation of add page working partially correct

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb://localhost/books229",
+  "CloudURI": "mongodb+srv://fasop4:fabian$0to@cluster0.ebj6o.mongodb.net/MyBooks?retryWrites=true&w=majority" 
+
 };
+
--- server/routes/books.js
+++ server/routes/books.js
@@ -10,11 +10,10 @@ let book = require('../models/books');
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
   // find all books in the books collection
-  book.find( (err, books) => {
+  book.find((err, books) => {
     if (err) {
       return console.error(err);
-    }
-    else {
+    } else {
       res.render('books/index', {
         title: 'Books',
         books: "",
@@ -28,30 +27,29 @@ router.get('/', (req, res, next) => {
 router.get('/add', (req, res, next) => {
 
   // show the details page to add a new book
-    res.render("details", {
-      title: "Add book",
-      books: ""
-    })
+  res.render("books/details", {
+    title: "Add book",
+    books: ""
+  })
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
 
   let newBook = new book({
-    title: req.body.Title,
-    description: body.Description,
-    price: req.body.Price,
-    author: req.body.Author,
-    genre: req.body.Genre
+    NameTextField: req.body.NameTextField,
+    PriceTextField: req.body.PriceTextField,
+    AuthorTextField: req.body.AuthorTextField,
+    GenreTextField: req.body.GenreTextField
   });
-  
+
   books.create(newBook, (err) => {
     if (err) {
       console.error(err);
       res.end(err);
     }
 
-    res.redirect("/books")
+    res.redirect("index")
   })
 
 });
@@ -59,62 +57,65 @@ router.post('/add', (req, res, next) => {
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-    let bookId = req.params.bookId;
+  let bookId = req.params.bookId;
 
-    books.findById(bookId, {}, {}, (err, bookToEdit) => {
-      if (err) {
-        console.error(err);
-        res.end(err);
-      }
+  books.findById(bookId, {}, {}, (err, bookToEdit) => {
+    if (err) {
+      console.error(err);
+      res.end(err);
+    }
 
-      res.render("details", {
-        title: "Edit Book",
-        books: bookToEdit,
-      });
+    res.render("books/details", {
+      title: "Edit Book",
+      books: bookToEdit,
     });
+  });
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
 
-    let bookId = req.params.bookId;
+  let bookId = req.params.bookId;
 
-    console.log(bookId);
-    console.log(req.body);
+  console.log(bookId);
+  console.log(req.body);
 
-    let updatedBook = new book({
-      _id: bookId,
-      title: req.body.Title,
-      description: body.Description,
-      price: req.body.Price,
-      author: req.body.Author,
-      genre: req.body.Genre
-    });
+  let updatedBook = new book({
+    _id: bookId,
+    NameTextField: req.body.NameTextField,
+    PriceTextField: req.body.PriceTextField,
+    AuthorTextField: req.body.AuthorTextField,
+    GenreTextField: req.body.GenreTextField
+  });
 
-    book.updateOne({_id : bookId}, updatedBook, {}, (err) => {
-      if (err) {
-        console.error(err);
-        res.end(err);
-      }
+  book.updateOne({
+    _id: bookId
+  }, updatedBook, {}, (err) => {
+    if (err) {
+      console.error(err);
+      res.end(err);
+    }
 
-      res.redirect("/books")
-    })
+    res.redirect("books/index")
+  })
 
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
 
-    let bookId = req.params.bookId;
+  let bookId = req.params.bookId;
 
-    book.remove({_id: bookId}, (err) => {
-      if (err) {
-        console.error(err);
-        res.end(err);
-      }
+  book.remove({
+    _id: bookId
+  }, (err) => {
+    if (err) {
+      console.error(err);
+      res.end(err);
+    }
 
-      res.redirect("/books")
-    });
+    res.redirect("books/index")
+  });
 });
 
 
